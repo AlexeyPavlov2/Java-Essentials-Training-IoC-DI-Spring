d8855bfd903fe5c979365be75cdd503072ad3f63
AlexeyPavlov2 <alexey.pavlov2@gmail.com>
2018-08-17 23:36:12 +0400

    JPARepository, HSQLDB, JavaConfig simple example

---
+++ JPARepository, HSQLDB, JavaConfig simple example/src/main/java/com/spring/training/entity/Address.java
@@ -0,0 +1,65 @@
+package com.spring.training.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "post_code")
+    private String postCode;
+
+    public Address() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", country='" + country + '\'' +
+                ", street='" + street + '\'' +
+                ", postCode='" + postCode + '\'' +
+                '}';
+    }
+}
